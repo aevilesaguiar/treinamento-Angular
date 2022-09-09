b718d8d673bc98544b6fbb6cfb9dce343dfdbe87
aevilesaguiar <aeviles@hotmail.com>
2022-09-09 10:25:10 -0300

    finalizado header footer

---
+++ conteudo/modeloDeRelatorioIdeacao.md
@@ -0,0 +1,23 @@
+# Modelo de Relatório de Ideação
+título do projeto
+
+1. Introdução
+Uma breve introdução à ideia do seu site. Informe os objetivos do projeto.
+
+Os valores/benefícios (tangíveis e intangíveis) que esta aplicação pode trazer para uma empresa/organização/usuário final.
+
+2. Lista Esperada de Recursos
+Uma breve lista de recursos que você espera que seu site suporte.
+
+Justificativas breves para incluir esses recursos.
+
+3. Pesquisa de mercado
+Faça uma pesquisa na Web para encontrar cerca de cinco sites que possam ter ideias semelhantes às suas.
+
+Compare brevemente os recursos desses aplicativos com sua ideia de aplicativo.
+
+4. Referências
+Dê referências a qualquer material/sites/livros etc. relevantes para sua ideia de aplicação
+
+Dê os links para os sites relevantes para sua ideia, que você listou na seção acima.
+
---
+++ src/app/app.module.ts
@@ -0,0 +1,54 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+
+
+import 'hammerjs';
+import { MenuComponent } from './menu/menu.component';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule ,
+    MatToolbarModule,
+    MatListModule,
+    CommonModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule,
+    FontAwesomeModule
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+
+}
